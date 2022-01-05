96ddbbe0f51e7098b69b3610b4d50a1afa1a9c54
GUSTAVO GOULART DE MELLO FARIAS <53410368+gustavogmfarias@users.noreply.github.com>
2022-01-05 09:51:42 -0300

    Desafio em andamento

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,23 @@
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+
 export function Content() {
-  // Complete aqui
-}
+  interface MovieProps {
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+  
+  useEffect(() => {
+  api
+  .get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`)
+  .then((response) => {
+    setMovies(response.data);
+  })
+  }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,29 @@
+import { useState, useEffect } from "react";
+import { api } from "../services/api";
+
 export function SideBar() {
-  // Complete aqui
+  interface GenreResponseProps {
+    id: number;
+    name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+    title: string;
+  }
+
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>(
+    {} as GenreResponseProps
+  );
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>("genres").then((response) => {
+      setGenres(response.data);
+    });
+  }, []);
+  useEffect(() => {
+    api
+      .get<GenreResponseProps>(`genres/${selectedGenreId}`)
+      .then((response) => {
+        setSelectedGenre(response.data);
+      });
+  }, [selectedGenreId]);
 }
